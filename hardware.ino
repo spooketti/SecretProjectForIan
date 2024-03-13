c88a412672b99d1d1618f41950da3989884fae2a
spooketti <jonliu168@gmail.com>
2024-03-13 14:08:23 -0700

    writeMessage semi complete: consider breaking sections smaller to be letter based due to 200ms

--- hardware.ino
+++ hardware.ino
@@ -10,6 +10,14 @@ int yValue = 0;
 String XString, YString = "";
 LiquidCrystal device(12, 11, 5, 4, 3, 2);
 int LEDList[4] = {RED,BLUE,YELLOW,GREEN}; //IMPORTANT!!!!!!
+bool isComplete = false;
+int section = 0;
+
+//expected serial input:
+// //redBool, blueBool, yellowBool, greenBool, completedBool
+//possibly compress data down to just 0s and 1s
+//red blue yellow and green control leds
+//completed should control whether or not leds shoudl print
 
 void setup() 
 {
@@ -34,15 +42,46 @@ void controller()
   Serial.print(XString + "," + YString);
 }
 
-void writeMessage()
+void writeMessage(int section)
 {
-  device.setCursor(0,1);
-  device.print("DEBUG IAN");
+  switch(section)
+  {
+    case 1:
+    device.setCursor(0,0);
+  	device.print("would");
+    break;
+    
+    case 2:
+    device.setCursor(6,0);
+  	device.print("you");
+    break;
+    
+    case 3:
+    device.setCursor(10,0);
+  	device.print("AA");
+    break;
+    
+    case 4:
+    device.setCursor(13,0);
+  	device.print("BB");
+    break;
+    
+    case 5:
+    device.setCursor(0,1);
+  	device.print("CD?");
+    isComplete = false;
+    break;
+  }
 }
 
 void loop() 
 {
   controller();
+  if(isComplete)
+  {
+    section++;
+    writeMessage()
+  }
   delay(200);
 }
 
